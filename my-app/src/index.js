8d87af63572bb97b4c1f43819f6d801eae7afacf
Edinson Rivera <edirju@gmail.com>
2022-11-08 15:43:06 -0500

    Eliminado archivos

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/index';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//const modal = ReactDOM.createPortal(document.getElementById('modal'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
